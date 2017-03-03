fde844f7b3daa632e915c0d2e592b2045b00b573
Daniel Thomas <drt24@cam.ac.uk>
2017-03-03 11:29:52 +0000

    Auto-fix puppet-lint problems

--- manifests/init.pp
+++ manifests/init.pp
@@ -20,36 +20,36 @@
 # Class for monkeysphere management
 #
 
-class monkeysphere ($keyserver = "pgp.mit.edu" ) {
+class monkeysphere ($keyserver = 'pgp.mit.edu' ) {
   # The needed packages
-  package { monkeysphere: ensure => installed, }
+  package { 'monkeysphere': ensure => installed, }
   
-  file { "monkeysphere_conf":
-    path => "/etc/monkeysphere/monkeysphere.conf",
-    mode => 644,
-    ensure => present,
+  file { 'monkeysphere_conf':
+    path    => '/etc/monkeysphere/monkeysphere.conf',
+    mode    => '0644',
+    ensure  => present,
     require => Package['monkeysphere'],
-    content => template("monkeysphere/monkeysphere.conf.erb"),
+    content => template('monkeysphere/monkeysphere.conf.erb'),
   }
-  file { "monkeysphere_host_conf":
-    path => "/etc/monkeysphere/monkeysphere-host.conf",
-    mode => 644,
-    ensure => present,
+  file { 'monkeysphere_host_conf':
+    path    => '/etc/monkeysphere/monkeysphere-host.conf',
+    mode    => '0644',
+    ensure  => present,
     require => Package['monkeysphere'],
-    content => template("monkeysphere/monkeysphere-host.conf.erb"),
+    content => template('monkeysphere/monkeysphere-host.conf.erb'),
   }
-  file { "monkeysphere_authentication_conf":
-    path => "/etc/monkeysphere/monkeysphere-authentication.conf",
-    mode => 644,
-    ensure => present,
+  file { 'monkeysphere_authentication_conf':
+    path    => '/etc/monkeysphere/monkeysphere-authentication.conf',
+    mode    => '0644',
+    ensure  => present,
     require => Package['monkeysphere'],
-    content => template("monkeysphere/monkeysphere-authentication.conf.erb"),
+    content => template('monkeysphere/monkeysphere-authentication.conf.erb'),
   }
-  file { "ms-subkey-exists":
-    path => "/usr/local/sbin/ms-subkey-exists",
-    mode => 755,
+  file { 'ms-subkey-exists':
+    path   => '/usr/local/sbin/ms-subkey-exists',
+    mode   => '0755',
     ensure => present,
-    source => "puppet:///modules/monkeysphere/ms-subkey-exists",
+    source => 'puppet:///modules/monkeysphere/ms-subkey-exists',
   }
   file { '/usr/local/sbin/ms-trust-certifiers':
     ensure => file,
@@ -69,34 +69,34 @@ define monkeysphere::import_key ( $scheme = 'ssh://', $port = '', $path = '/etc/
   # if we're getting a port number, prefix with a colon so it's valid
   $prefixed_port = $port ? {
     '' => '',
-    default => ":$port"
+    default => ":${port}"
   }
 
   $key = "${scheme}${hostname}${prefixed_port}"
 
-  exec { "monkeysphere-host import-key $path $key":
-    alias => "monkeysphere-import-key",
-	  require => [ Package["monkeysphere"],  File["monkeysphere_host_conf"] ],
-	  unless => "/usr/sbin/monkeysphere-host s | grep $key > /dev/null"
+  exec { "monkeysphere-host import-key ${path} ${key}":
+    alias  => 'monkeysphere-import-key',
+    require => [ Package['monkeysphere'],  File['monkeysphere_host_conf'] ],
+    unless => "/usr/sbin/monkeysphere-host s | grep ${key} > /dev/null"
   }
   
 }
 
 define monkeysphere::publish_server_keys {
-  exec { "monkeysphere-host-publish-keys":
-    command => "monkeysphere-host publish-keys",
-    unless  => "gpg --logger-fd 1 --keyserver=${::ms_keyserver} --recv-key `sudo monkeysphere-host show-keys | grep OpenPGP | sed 's/OpenPGP fingerprint: //'` | grep 'Total number processed: 1' >/dev/null || echo false",
-    environment => "MONKEYSPHERE_PROMPT=false",
-    require => [ Package["monkeysphere"], Exec["monkeysphere-import-key"], File["monkeysphere_host_conf"] ],
+  exec { 'monkeysphere-host-publish-keys':
+    command     => 'monkeysphere-host publish-keys',
+    unless      => "gpg --logger-fd 1 --keyserver=${::ms_keyserver} --recv-key `sudo monkeysphere-host show-keys | grep OpenPGP | sed 's/OpenPGP fingerprint: //'` | grep 'Total number processed: 1' >/dev/null || echo false",
+    environment => 'MONKEYSPHERE_PROMPT=false',
+    require     => [ Package['monkeysphere'], Exec['monkeysphere-import-key'], File['monkeysphere_host_conf'] ],
   }
 }
 
 # add certifiers
 define monkeysphere::add_id_certifier( $keyid ) {
-  exec { "monkeysphere-authentication add-id-certifier $keyid && monkeysphere-authentication update-users":
-	  environment => "MONKEYSPHERE_PROMPT=false",
-	  require => [ Package["monkeysphere"], File["monkeysphere_authentication_conf"] ],
-	  unless => "/usr/sbin/monkeysphere-authentication list-id-certifiers | grep $keyid > /dev/null"
+  exec { "monkeysphere-authentication add-id-certifier ${keyid} && monkeysphere-authentication update-users":
+    environment => 'MONKEYSPHERE_PROMPT=false',
+    require    => [ Package['monkeysphere'], File['monkeysphere_authentication_conf'] ],
+    unless     => "/usr/sbin/monkeysphere-authentication list-id-certifiers | grep ${keyid} > /dev/null"
   }
 }
 
@@ -109,26 +109,26 @@ define monkeysphere::authorized_user_ids( $user_ids,  $dest_dir = '/root/.monkey
 
   file {
     $dest_dir:
-      owner => $user,
-      group => $calculated_group,
-      mode => 755,
+      owner  => $user,
+      group  => $calculated_group,
+      mode   => '0755',
       ensure => directory,
   }
 
   file {
     "${dest_dir}/${dest_file}":
-      owner => $user,
-      group => $calculated_group,
-      mode => 644,
+      owner   => $user,
+      group   => $calculated_group,
+      mode    => '0644',
       content => template('monkeysphere/authorized_user_ids.erb'),
-      ensure => $ensure,
-      require => File[$dest_dir] 
+      ensure  => $ensure,
+      require => File[$dest_dir]
   }
 
-  exec { "monkeysphere-authentication update-users $user":
+  exec { "monkeysphere-authentication update-users ${user}":
     refreshonly => true,
-    require => [ File["monkeysphere_authentication_conf"], Package["monkeysphere"] ],
-    subscribe => File["${dest_dir}/${dest_file}"] 
+    require     => [ File['monkeysphere_authentication_conf'], Package['monkeysphere'] ],
+    subscribe   => File["${dest_dir}/${dest_file}"]
   }
 }
 
@@ -136,7 +136,7 @@ define monkeysphere::authorized_user_ids( $user_ids,  $dest_dir = '/root/.monkey
 # in the monkeysphere. This is intended to be the same as generated a
 # password-less ssh key. Depends on gpg module and gpg::private_key 
 #
-define monkeysphere::auth_capable_user ( $passphrase, $pseudo_random = false, $home = '') { 
+define monkeysphere::auth_capable_user ( $passphrase, $pseudo_random = false, $home = '') {
 
   $user = $title
   if $home == '' {
@@ -146,12 +146,12 @@ define monkeysphere::auth_capable_user ( $passphrase, $pseudo_random = false, $h
   }
 
   # handle auth subkey
-  exec { "monkeysphere-gen-subkey-$user":
-    command     => "printf '$passphrase\n' | monkeysphere gen-subkey",
-    require     => [ Package["monkeysphere"], Exec["gpg-pem2openpgp-$user" ] ],
+  exec { "monkeysphere-gen-subkey-${user}":
+    command     => "printf '${passphrase}\n' | monkeysphere gen-subkey",
+    require     => [ Package['monkeysphere'], Exec["gpg-pem2openpgp-${user}" ] ],
     user        => $user,
     environment => ["HOME=${real_home}"],#otherwise HOME not set
-    unless      => "/usr/local/sbin/ms-subkey-exists",
+    unless      => '/usr/local/sbin/ms-subkey-exists',
   }
 
 }
@@ -159,21 +159,21 @@ define monkeysphere::auth_capable_user ( $passphrase, $pseudo_random = false, $h
 # use systemd to maintain - TODO - only works for root user now
 define monkeysphere::ssh_agent( $passphrase, $ensure = 'running' ) {
   # protected directory to store the ssh-agent socket
-  file { "/root/.ssh-agent-socket":
-    ensure => "directory",
-    mode => 700,
+  file { '/root/.ssh-agent-socket':
+    ensure => 'directory',
+    mode   => '0700',
   }
 
   file { '/etc/systemd/system/ssh-agent-root.service':
-    ensure => file,
-    mode => 755,
-    content => template("monkeysphere/ssh-agent-root.service.erb"),
-    owner => "root",
-    require => Exec["monkeysphere-gen-subkey-root"]
+    ensure  => file,
+    mode    => '0755',
+    content => template('monkeysphere/ssh-agent-root.service.erb'),
+    owner   => 'root',
+    require => Exec['monkeysphere-gen-subkey-root']
   }
 
   service {'ssh-agent-root':
-    ensure => 'running',
+    ensure  => 'running',
     require => File['/etc/systemd/system/ssh-agent-root.service']
   }
 }
@@ -184,7 +184,7 @@ define monkeysphere::trusting_user( $passphrase, $home ) {
   $user = $name
   exec { "ms-trust-certifiers-${user}":
     command => "ms-trust-certifiers ${user}",
-    unless  => "ms-does-trust-certifiers ${user}", 
+    unless  => "ms-does-trust-certifiers ${user}",
     require => File['/usr/local/sbin/ms-trust-certifiers','/usr/local/sbin/ms-does-trust-certifiers'],
   }
   # Check we have up to date keys daily
@@ -196,10 +196,10 @@ define monkeysphere::trusting_user( $passphrase, $home ) {
   }
   # Specify the use of the monkeysphere ssh-proxycommand
   file { "${home}.ssh/config":
-    ensure => file,
-    owner  => $user,
-    group  => $user,
-    mode   => '0600',
+    ensure  => file,
+    owner   => $user,
+    group   => $user,
+    mode    => '0600',
     require => File["${home}.ssh/"],
   }
   augeas {"monkeysphere-proxycommand-${user}":
--- manifests/sshd.pp
+++ manifests/sshd.pp
@@ -1,5 +1,5 @@
 class monkeysphere::sshd::default(
-  $max_startups                      = "",
+  $max_startups                      = '',
   $listen_address                    = [ '0.0.0.0', '::' ],
   $agent_forwarding                  = 'no',
   $tcp_forwarding                    = 'no',
@@ -7,69 +7,69 @@ class monkeysphere::sshd::default(
   $use_pam                           = 'no'
 )
 {
-  class { "monkeysphere::sshd":
-    authorized_keys_file             => "/var/lib/monkeysphere/authorized_keys/%u",
-    max_startups                     => $max_startups,
-    agent_forwarding                 => $agent_forwarding,
-    tcp_forwarding                   => $tcp_forwarding,
-    x11_forwarding                   => $x11_forwarding,
-    listen_address                   => $listen_address,
-    use_pam                          => $use_pam,
+  class { 'monkeysphere::sshd':
+    authorized_keys_file => '/var/lib/monkeysphere/authorized_keys/%u',
+    max_startups         => $max_startups,
+    agent_forwarding     => $agent_forwarding,
+    tcp_forwarding       => $tcp_forwarding,
+    x11_forwarding       => $x11_forwarding,
+    listen_address       => $listen_address,
+    use_pam              => $use_pam,
   }
 }
 
 class monkeysphere::sshd::pam(
-  $max_startups                      = "",
+  $max_startups                      = '',
   $listen_address                    = [ '0.0.0.0', '::' ],
   $agent_forwarding                  = 'no'
 )
 {
-  class { "monkeysphere::sshd": 
-    authorized_keys_file             => "/var/lib/monkeysphere/authorized_keys/%u",
+  class { 'monkeysphere::sshd':
+    authorized_keys_file              => '/var/lib/monkeysphere/authorized_keys/%u',
     challenge_response_authentication => 'yes',
-    password_authentication          => 'yes',
-    use_pam                          => 'yes',
-    allowed_groups                   => "sshusers root",
-    tail_additional_options          => "MaxAuthTries 6",
-    login_grace_time                 => 60,
-    tcp_forwarding                   => yes,
-    max_startups                     => $max_startups,
-    agent_forwarding                 => $agent_forwarding,
+    password_authentication           => 'yes',
+    use_pam                           => 'yes',
+    allowed_groups                    => 'sshusers root',
+    tail_additional_options           => 'MaxAuthTries 6',
+    login_grace_time                  => 60,
+    tcp_forwarding                    => yes,
+    max_startups                      => $max_startups,
+    agent_forwarding                  => $agent_forwarding,
   }
 }
 
 class monkeysphere::sshd (
   $listen_address                    = [ '0.0.0.0', '::' ],
   $allowed_users                     = '',
-  $allowed_groups                    = '', 
+  $allowed_groups                    = '',
   $use_pam                           = 'no',
   $use_dns                           = 'yes',
-  $permit_root_login                 = 'without-password', 
-  $login_grace_time                  = '600', 
-  $password_authentication           = 'no', 
+  $permit_root_login                 = 'without-password',
+  $login_grace_time                  = '600',
+  $password_authentication           = 'no',
   $kerberos_authentication           = 'no',
   $kerberos_orlocalpasswd            = 'yes',
   $kerberos_ticketcleanup            = 'yes',
-  $gssapi_authentication             = 'no', 
-  $gssapi_cleanupcredentials         = 'yes', 
+  $gssapi_authentication             = 'no',
+  $gssapi_cleanupcredentials         = 'yes',
   $tcp_forwarding                    = 'no',
-  $x11_forwarding                    = 'no', 
-  $agent_forwarding                  = 'no', 
+  $x11_forwarding                    = 'no',
+  $agent_forwarding                  = 'no',
   $challenge_response_authentication = 'no',
-  $pubkey_authentication             = 'yes', 
+  $pubkey_authentication             = 'yes',
   $rsa_authentication                = 'no',
   $strict_modes                      = 'yes',
-  $ignore_rhosts                     = 'yes', 
+  $ignore_rhosts                     = 'yes',
   $rhosts_rsa_authentication         = 'no',
-  $hostbased_authentication          = 'no', 
-  $permit_empty_passwords            = 'no', 
-  $authorized_keys_file              = "%h/.ssh/authorized_keys",
-  $sftp_subsystem                    = '', 
-  $head_additional_options           = '', 
-  $tail_additional_options           = '', 
-  $ensure_version                    = "present",
-  $ports                             = [ "22" ],
-  $max_startups                      = ""
+  $hostbased_authentication          = 'no',
+  $permit_empty_passwords            = 'no',
+  $authorized_keys_file              = '%h/.ssh/authorized_keys',
+  $sftp_subsystem                    = '',
+  $head_additional_options           = '',
+  $tail_additional_options           = '',
+  $ensure_version                    = 'present',
+  $ports                             = [ '22' ],
+  $max_startups                      = ''
 )
 {
   $rsa_key_exists   = exists('/etc/ssh/ssh_host_rsa_key')
@@ -78,10 +78,10 @@ class monkeysphere::sshd (
 
   file { 'sshd_config':
     path    => '/etc/ssh/sshd_config',
-    content => template("monkeysphere/sshd/sshd.conf.erb"),
+    content => template('monkeysphere/sshd/sshd.conf.erb'),
     notify  => Service[sshd],
     require => Package[openssh],
-    owner   => root, group => 0, mode => 600;
+    owner   => root, group => 0, mode => '0600';
   }
 
   file { '/etc/ssh/sshd_config.d':
@@ -99,8 +99,8 @@ class monkeysphere::sshd (
   }
 
   package { 'openssh':
-    name    => 'openssh-server',
-    ensure  => $ensure_version,
+    name   => 'openssh-server',
+    ensure => $ensure_version,
   }
   
   package {'openssh-client':
